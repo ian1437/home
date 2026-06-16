dfb1eee7584e10814d2da115b5c1be0fe1b98fd5
Mystery N <isimmons2012@fau.edu>
2026-06-16 02:22:09 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,75 @@
-/*!
-* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Forms show / hide
+    */
+    $('.show-register-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-login-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.login-form').fadeOut('fast', function(){
+    			$('.register-form').fadeIn('fast');
+    		});
+    	}
+    });
+    // ---
+    $('.show-login-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-register-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.register-form').fadeOut('fast', function(){
+    			$('.login-form').fadeIn('fast');
+    		});
+    	}
+    });
+    
+    /*
+        Login form validation
+    */
+    $('.l-form input[type="text"], .l-form input[type="password"], .l-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.l-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.r-form input[type="text"], .r-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.r-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
